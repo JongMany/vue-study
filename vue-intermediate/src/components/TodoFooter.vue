e23239595a044ebe37163f7304f561c41753dcde
JongMany <blackberry1114@naver.com>
2025-01-30 20:28:11 +0900

    Refactor: AppModal

--- vue-intermediate/src/components/TodoFooter.vue
+++ vue-intermediate/src/components/TodoFooter.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="clearAllContainer">
-    <span class="clearAllBtn" @click="clearTodo">Clear All</span>
+    <span class="clearAllBtn" @click="showModal = true">Clear All</span>
+    <!-- Modal -->
+    <app-modal v-if="showModal" @close="showModal = false">
+      <template #header> <h3>정말로 삭제하시겠습니까?</h3> </template>
+      <template #body>
+        <p>삭제 후에는 복구가 불가능합니다.</p>
+        <div class="modalBtnContainer">
+          <span class="cancelBtn" @click="showModal = false">닫기</span>
+          <span class="confirmBtn" @click="clearTodo">삭제</span>
+        </div>
+      </template>
+    </app-modal>
   </div>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+import AppModal from "./shared/AppModal.vue";
+
 export default {
+  data() {
+    return { showModal: false };
+  },
   methods: {
-    clearTodo() {
-      this.$store.commit("clearAllTodoItems");
-    },
+    ...mapMutations({ clearTodo: "clearAllTodoItems" }),
+  },
+  components: {
+    AppModal: AppModal,
   },
 };
 </script>
@@ -26,5 +44,25 @@ export default {
 .clearAllBtn {
   color: #e20303;
   display: block;
+}
+
+.modalBtnContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 60px;
+  height: 40px;
+}
+
+.cancelBtn {
+  border: none;
+  cursor: pointer;
+  color: #e20303;
+}
+
+.confirmBtn {
+  border: none;
+  cursor: pointer;
+  color: black;
 }
 </style>
